7e2ca8f51ec716499a21089ac4f7a47621c621bd
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-05-12 10:49:27 +0200

    updates route

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,50 +1,37 @@
-import { Button } from '@material-ui/core';
-import {
-  Container,
-  Titulo,
-  InputContainer
-} from './styles';
-import {
-  Input,
-  InputLabel,
-  InputAdornment 
-} from '@material-ui/core';
+import { Button } from "@material-ui/core";
+import { Container, Titulo, InputContainer } from "./styles";
+import { Input, InputLabel, InputAdornment } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 
-function Login() {
+function Login({ nome, setNome, saldo, setSaldo }) {
+  const history = useHistory();
   return (
     <Container>
-      <Titulo>
-        Insira o seu nome
-      </Titulo>
+      <Titulo>Insira o seu nome</Titulo>
       <InputContainer>
-        <InputLabel>
-          Nome
-        </InputLabel>
+        <InputLabel>Nome</InputLabel>
         <Input
+          value={nome}
+          onChange={(event) => setNome(event.target.value)}
           type="text"
         />
       </InputContainer>
       <InputContainer>
-        <InputLabel>
-          Saldo
-        </InputLabel>
+        <InputLabel>Saldo</InputLabel>
         <Input
-        type="number"
-        startAdornment={
-          <InputAdornment position="start">
-            R$
-          </InputAdornment>
-        }
-      />
+          type="number"
+          onChange={(event) => setSaldo(event.target.value)}
+          startAdornment={<InputAdornment position="start">R$</InputAdornment>}
+        />
       </InputContainer>
       <Button
         variant="contained"
         color="primary"
-      >
+        onClick={() => history.push("/feira")}>
         Avançar
       </Button>
     </Container>
-  )
-};
+  );
+}
 
 export default Login;
