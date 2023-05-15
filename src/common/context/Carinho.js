645a9ebadeef2cccbf60f152cb53d8c55d7a1c70
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-05-15 09:01:40 +0200

    Create Carinho.js
    new js file
    
    context

---
+++ src/common/context/Carinho.js
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+export const CarrinhoContext = createContext();
+CarrinhoContext.displayName = "Carrinho";
+
+export const CarrinhoProvider = ({ children }) => {
+  return <CarrinhoContext.Provider>
+    {children}
+  </CarrinhoContext.Provider>;
+};
