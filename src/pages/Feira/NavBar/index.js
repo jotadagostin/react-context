d4db3a7753a47f2d85f6ade6d0304b386104a818
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-05-16 10:10:47 +0200

    Update index.js
    navbar

--- src/pages/Feira/NavBar/index.js
+++ src/pages/Feira/NavBar/index.js
@@ -1,20 +1,21 @@
-import { Nav } from './styles';
-import { ReactComponent as Logo } from 'assets/logo.svg';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
+import { Nav } from "./styles";
+import { ReactComponent as Logo } from "assets/logo.svg";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import IconButton from "@material-ui/core/IconButton";
+import Badge from "@material-ui/core/Badge";
+import { useCarrinhoContext } from "common/context/Carinho";
 
 export default function NavBar() {
+  const { quantidadeProdutos } = useCarrinhoContext();
   return (
     <Nav>
       <Logo />
-      <IconButton>
-        <Badge
-          color="primary"
-        >
+      {quantidadeProdutos}
+      <IconButton disabled={quantidadeProdutos === 0}>
+        <Badge color="primary" badgeContent={quantidadeProdutos}>
           <ShoppingCartIcon />
         </Badge>
       </IconButton>
     </Nav>
-  )
+  );
 }
