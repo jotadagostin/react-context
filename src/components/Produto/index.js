3ff9103929bd922c5928285533774efc15a8005e
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-05-16 09:52:57 +0200

    Update index.js

--- src/components/Produto/index.js
+++ src/components/Produto/index.js
@@ -1,54 +1,49 @@
-import { Container } from "./styles";
-import { memo, useContext } from "react";
-import { IconButton } from "@material-ui/core";
-import AddIcon from "@material-ui/icons/Add";
-import RemoveIcon from "@material-ui/icons/Remove";
-import { CarrinhoContext } from "common/context/Carinho";
+import { Container } from './styles';
+import { memo } from 'react';
+import { IconButton } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
+import { useCarrinhoContext } from 'common/context/Carrinho';
 
-function Produto({ nome, foto, id, valor, unidade }) {
-  const { carrinho, setCarrinho, removerProduto } = useContext(CarrinhoContext);
 
-  function adicionarProduto(novoProduto) {
-    const temOProduto = carrinho.some(
-      (itemDoCarrinho) => itemDoCarrinho.id === novoProduto.id
-    );
-    if (!temOProduto) {
-      novoProduto.quantidade = 1;
-      return setCarrinho((carrinhoAnterior) => [
-        ...carrinhoAnterior,
-        novoProduto,
-      ]);
-    }
-    setCarrinho((carrinhoAnterior) =>
-      carrinhoAnterior.map((itemDoCarrinho) => {
-        if (itemDoCarrinho.id === novoProduto.id)
-          itemDoCarrinho.quantidade += 1;
-        return itemDoCarrinho;
-      })
-    );
-  }
-
+function Produto({
+  nome,
+  foto,
+  id,
+  valor,
+  unidade
+}) {
+  const { carrinho, adicionarProduto, removerProduto } = useCarrinhoContext();
+  const produtoNoCarrinho = carrinho.find(itemDoCarrinho => itemDoCarrinho.id === id);
   return (
-    <Container>
-      <div>
-        <img src={`/assets/${foto}.png`} alt={`foto de ${nome}`} />
-        <p>
-          {nome} - R$ {valor?.toFixed(2)} <span>Kg</span>
-        </p>
-      </div>
-      <div>
-        <IconButton color="secondary" onClick={() => removerProduto(id)}>
-          <RemoveIcon />
-        </IconButton>
-
-        <IconButton
-          color="primary"
-          onClick={() => adicionarProduto({ nome, foto, id, valor })}>
-          <AddIcon />
-        </IconButton>
-      </div>
-    </Container>
-  );
+      <Container>
+        <div>
+          <img
+            src={`/assets/${foto}.png`}
+            alt={`foto de ${nome}`}
+          />
+          <p>
+            {nome} - R$ {valor?.toFixed(2)} <span>Kg</span>
+          </p>
+        </div>
+        <div>
+          <IconButton
+            color="secondary"
+            onClick={() => removerProduto(id)}
+            disabled={!produtoNoCarrinho}
+          >
+            <RemoveIcon />
+          </IconButton>
+          {produtoNoCarrinho?.quantidade || 0}
+          <IconButton
+            color="primary"
+            onClick={() => adicionarProduto({ nome, foto, id, valor })}
+          >
+            <AddIcon />
+          </IconButton>
+        </div>
+      </Container>
+  )
 }
 
-export default memo(Produto);
+export default memo(Produto)
