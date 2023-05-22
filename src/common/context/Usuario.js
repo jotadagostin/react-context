28df125eef7073705594dc641201f50090cc1617
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-05-22 09:58:32 +0200

    Update Usuario.js

--- src/common/context/Usuario.js
+++ src/common/context/Usuario.js
@@ -1,9 +1,13 @@
+// criando um context
 import { createContext, useState } from "react";
 
 export const UsuarioContext = createContext();
 UsuarioContext.displayName = "Usuário";
 
-function UsuarioProvider({ children }) {
+// // criaremos um componente extra que terá a responsabilidade de salvar os states e disponibiliza-los para os outros componentes
+
+
+export const UsuarioProvider = ({ children }) => {
   const [nome, setNome] = useState("");
   const [saldo, setSaldo] = useState(0);
   return (
